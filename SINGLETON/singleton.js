88ed72711a9362164f7b51158e848416fde9ff0d
damaroja <ildefonsoarrecife3@hotmail.com>
2024-04-21 17:01:41 +0100

    Update singleton.js

--- SINGLETON/singleton.js
+++ SINGLETON/singleton.js
@@ -26,3 +26,59 @@ console.assert(instance1 !== instance2, 'Son objetos diferentes'); // true
 //En este caso, se crea una instancia de la clase Singleton y se asigna a la variable instance1. Luego, se crea otra instancia de la clase Singleton y se asigna a la variable instance2. Al verificar si instance1 y instance2 son iguales, se obtiene false, lo que significa que son objetos diferentes. Esto demuestra que el patrón de diseño singleton garantiza que solo haya una instancia de la clase Singleton en todo el programa.
 
 // Para ejecutar este código, simplemente copie y pegue el código en un archivo llamado app.js y ejecútelo con Node.js. Debería ver la salida "Son objetos diferentes" en la consola.
+
+
+
+
+
+
+function SingletonFoo() {
+
+    let fooInstance = null;
+ 
+    // For our reference, let's create a counter that will track the number of active instances
+    let count = 0;
+ 
+    function printCount() {
+        console.log("Number of instances: " + count);
+    }
+ 
+    function init() {
+        // For our reference, we'll increase the count by one whenever init() is called
+        count++;
+ 
+        // Do the initialization of the resource-intensive object here and return it
+        return {}
+    }
+ 
+    function createInstance() {
+        if (fooInstance == null) {
+            fooInstance = init();
+        }
+        return fooInstance;
+    }
+ 
+    function closeInstance() {
+        count--;
+        fooInstance = null;
+    }
+ 
+    return {
+        initialize: createInstance,
+        close: closeInstance,
+        printCount: printCount
+    }
+ }
+ 
+ let foo = SingletonFoo();
+ 
+ foo.printCount() // Prints 0
+ foo.initialize()
+ foo.printCount() // Prints 1
+ foo.initialize()
+ foo.printCount() // Still prints 1
+ foo.initialize()
+ foo.printCount() // Still 1
+ foo.close()
+ foo.printCount() // Prints 0
+
